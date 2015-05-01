b75eff9ed63fedca39af6270a595c46e5cf128ad
Evan Thompson <evanmt13@gmail.com>
2015-05-01 15:30:08 -0400

    Added some skeleton to the server.cpp

--- server.cpp
+++ server.cpp
@@ -3,7 +3,34 @@
 #include <cstdlib>
 #include <iostream>
 
+
+//Thread functions go in this section
+//-------------- THREAD ----------------
+
+
+
+
+
+//--------------------------------------
+
+
+
+//sets up the server and gets everything ready to start up, also sets up the paging system
+void init_server(){
+
+}
+//start listening for connections and then starts up threads to handle the commands issued
+void start_listening(){
+	
+}
+
 int main(){
+
+  init_server();
+  
+  start_listening();  
+
+
   std::cout<<"Hello World!"<<std::endl;
   return EXIT_SUCCESS;
 }
