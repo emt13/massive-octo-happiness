7bd0b8787b60e0904728b8aa30438305474252b6
Tausif Ahmed <atausif00@gmail.com>
2015-05-13 15:47:34 -0400

    Added test client files and outlined implementations

---
+++ client_c++.cpp
@@ -0,0 +1,28 @@
+/* client.cpp  */
+
+//Names: Evan Thompson, John Cusick, Tausif Ahmed
+
+/*NOTES:
+ *	Test client implemented in C++
+ *
+ * */
+
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+#define PORT_NUM 8765
+
+int main() {
+
+	/*
+	 *	- READ IN TEST FILE
+	 *	- Store commands into vector
+	 *	- Connect to server via PORT_NUM
+	 *	- Read through vector and pass each command to server
+	 *
+	 * */
+}
---
+++ client_java.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+
+public class client_java {
+	final static int PORT_NUM = 8765;
+	static ArrayList<String> commands = new ArrayList<String>();
+
+	public static void main (String args[]) {
+		/*
+		 *	- Read in test file using Scanner
+		 *	- Store line commands into arraylist
+		 *	- open up server on PORT_NUM
+		 *	- Iterate through ArrayList and send commands to server
+		 *
+		 * 
+		 */
+	}
+}	
